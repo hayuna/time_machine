99c6294cfc4129eb9e02362f86e2076ea0f03a3a
Unknown <marek.kowalonek@gmail.com>
2017-12-16 16:23:05 +0100

    v0.9.1

--- projekt.sql
+++ projekt.sql
@@ -1,9 +1,27 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `users` (`id` int(11) NOT NULL, `login` varchar(100) NOT NULL, `pass` varchar(100) NOT NULL) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+CREATE TABLE IF NOT EXISTS `tasks` (
+	`Task_ID` int(11) NOT NULL,
+	`Name_of_task` varchar(50) NOT NULL,
+	`Start_task` datetime NOT NULL,
+	`End_task` datetime NOT NULL,
+	`user` varchar(15) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
-INSERT INTO `users` (`id`, `login`, `pass`) VALUES (1, 'John', 'John');
+CREATE TABLE IF NOT EXISTS `users` (
+	`User_ID` int(11) NOT NULL,
+  	`Name` varchar(20) NOT NULL,
+  	`Surname` varchar(20) NOT NULL,
+  	`Login` varchar(15) NOT NULL,
+  	`Password` varchar(15) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
-ALTER TABLE `users` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `login` (`login`);
-ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+INSERT INTO `users` (`User_ID`, `Name`, `Surname`, `Login`, `Password`) VALUES
+	(1, 'John', 'Doe', 'John', 'John'),
+	(2, 'Jan', 'Kowalski', 'admin', 'admin');
+
+ALTER TABLE `tasks` ADD PRIMARY KEY (`Task_ID`);
+ALTER TABLE `users` ADD PRIMARY KEY (`User_ID`), ADD UNIQUE KEY `login` (`Name`);
+ALTER TABLE `tasks` MODIFY `Task_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+ALTER TABLE `users` MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
